9f6325a14e56024a7ce7fc12ae71c5552a80016d
dreamtech11 <jacksonifiok18@gmail.com>
2023-01-04 09:56:32 -0800

    loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,15 +10,12 @@ int main(void)
 	int i;
 
 	printf("infinite loop incoming :(/n");
-
 	i = 0
 
 	/*while (i < 10) */
 	/*{ */
 	/*       putchar(i);*/
 	/*} */
-	
 	printf("infinite loop avoided! \\0/\n");
-
 	return  (0);
 }
